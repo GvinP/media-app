2d947c55f1871fd03158d3ff5d77562e9dafbe9d
kudryashovalexey <gvinpin72@gmail.com>
2023-04-08 11:51:38 +0300

    amplify config

--- App.tsx
+++ App.tsx
@@ -1,4 +1,8 @@
 import Navigation from './src/navigation';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+
+Amplify.configure(awsconfig);
 
 const App = () => {
   return <Navigation />;
