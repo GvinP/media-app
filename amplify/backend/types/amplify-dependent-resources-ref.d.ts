b297d2299d503de035df20c929bf731519cfcfea
kudryashovalexey <gvinpin72@gmail.com>
2023-04-23 11:39:48 +0300

    setup storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -28,5 +28,11 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     }
+  },
+  "storage": {
+    "storage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
